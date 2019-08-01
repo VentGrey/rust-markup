d3c838d8ee9831506904355f15ef8f91833ec6b1
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-01 17:40:01 -0500

    making function prototypes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+enum State {
+    Normal,
+    Comment,
+    Upper,
+    Lower
+}
+
+fn machine_cycle(state: &MachineState, c: chat) -> (Option<char>, MachineState) {
+   
 }
