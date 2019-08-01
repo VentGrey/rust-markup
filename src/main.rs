473af9144a1c5e49c2a99296df51d47845300bf3
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-01 17:58:58 -0500

    It works!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone)]
 enum State {
     Normal,
     Comment,
@@ -5,6 +6,37 @@ enum State {
     Lower
 }
 
-fn machine_cycle(state: &MachineState, c: chat) -> (Option<char>, MachineState) {
-   
+fn machine_cycle(state: State, c: char) -> (Option<char>, State) {
+    use self::State::*;
+    match (state, c) {
+        (Normal, '#') => (None, Comment),
+        (Normal, '^') => (None, Upper),
+        (Normal, '_') => (None, Lower),
+        (Normal, other) => (Some(other), Normal),
+        (Comment, '#') => (None, Normal),
+        (Comment, _) => (None, Comment),
+        (Upper, '^') => (None, Normal),
+        (Upper, other) => (Some(other.to_ascii_uppercase()), Upper),
+        (Lower, '_') => (None, Normal),
+        (Lower, other) => (Some(other.to_ascii_lowercase()), Lower),
+    }
+}
+
+fn main() {
+    let mut state = State::Normal;
+    let mut processed_string = String::new();
+
+    let input = "This _IS_ some ^testing^ #shit ";
+
+    for character in input.chars() {
+        let (output, new_state) = machine_cycle(state, character);
+
+        if let Some(c) = output {
+            processed_string.push(c);
+        }
+
+        state = new_state;
+    }
+
+    println!("{}", processed_string);
 }
